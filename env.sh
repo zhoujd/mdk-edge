984214cd8932b86db1aaff30ce9f0b3f34809928
Zachary Zhou <zchrzhou@gmail.com>
2022-07-18 21:44:50 +0800

    clean code

--- env.sh
+++ env.sh
@@ -2,9 +2,6 @@
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
-## sudo yum install redhat-lsb-core
-## sudo pacman -S lsb-release
-
 add_path() {
     ARG=$1
     if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
