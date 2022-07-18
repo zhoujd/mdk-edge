8d18f8e6959ccbd3df06a3e864cacf52f113de43
Zachary Zhou <zchrzhou@gmail.com>
2022-07-18 21:03:48 +0800

    more for env.sh

--- env.sh
+++ env.sh
@@ -1,4 +1,25 @@
 ## env.sh
 
-export SINGLE_HOST=127.0.0.1
-export DEK_HOME=`pwd`
+export SINGLE_HOST=${SINGLE_HOST:-127.0.0.1}
+export DEK_HOME=${DEK_HOME:-`pwd`}
+
+config() {
+    local file=$1
+    shift
+    local vars=("$@")
+    for i in "${vars[@]}"; do
+        echo "[$file][update] $i"
+        yq -i $i $file
+    done
+}
+
+query() {
+    local file=$1
+    shift
+    local vars=("$@")
+    for i in "${vars[@]}"; do
+        key=(${i//=/ })
+        value=`yq ${key[0]} $file`
+        echo "[$file][query] $key=$value"
+    done
+}
