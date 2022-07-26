e32df7263e5169f9da562cef9af99c3bba3858b7
Zachary Zhou <zchrzhou@gmail.com>
2022-07-26 11:14:03 +0800

    add single & multi node auto check

--- case/sg2/env.sh
+++ case/sg2/env.sh
@@ -4,8 +4,15 @@
 export CONTROLLER_HOST=${CONTROLLER_HOST:-127.0.0.1}
 export NODE_HOST=${NODE_HOST:-127.0.0.1}
 export DEK_HOME=${DEK_HOME:-`pwd`}
+export SINGLE_FLAG=false
+
+## single node deployment
+if [ "$CONTROLLER_HOST" = "$NODE_HOST" ]; then
+    SINGLE_FLAG=true
+fi
+
 INVENTORY=(
-    '.all.vars.single_node_deployment=true'
+    '.all.vars.single_node_deployment=strenv(SINGLE_FLAG)'
     '.controller_group.hosts.controller.ansible_host=strenv(CONTROLLER_HOST)'
     '.edgenode_group.hosts.node01.ansible_host=strenv(NODE_HOST)'
 )
@@ -16,7 +23,7 @@ export HTTP_PROXY=${HTTPS_PROXY:-""}
 export FTP_PROXY=${FTP_PROXY:-""}
 export NO_PROXY=${NO_PROXY:-".intel.com,intel.com,localhost,127.0.0.0/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16"}
 VARS=(
-    ## single node
+    ## dek
     '.proxy_env.http_proxy=strenv(HTTP_PROXY)'
     '.proxy_env.https_proxy=strenv(HTTP_PROXY)'
     '.proxy_env.ftp_proxy=strenv(FTP_PROXY)'
