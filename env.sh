630bdb1b823a2d718728f22d03885ef2543dc34c
Zachary Zhou <zchrzhou@gmail.com>
2022-07-29 09:34:21 +0800

    more for env.sh

--- env.sh
+++ env.sh
@@ -3,11 +3,12 @@
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 add_path() {
-    ARG=$1
-    if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
-        PATH="$ARG${PATH:+":$PATH"}"
-    fi
-
+    for ARG in "$@"
+    do
+        if [ -d "$ARG" ] && [[ ":$PATH:" != *":$ARG:"* ]]; then
+            PATH="$ARG${PATH:+":$PATH"}"
+        fi
+    done
     export PATH
 }
 
